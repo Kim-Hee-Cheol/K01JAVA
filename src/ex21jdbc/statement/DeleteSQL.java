c3ae17513017c2eec5d4f8782ef199adf898fae2
Kosmo_07 <Kosmo_07@DESKTOP-875EV6J>
2020-03-24 18:07:10 +0900

    첫번째 커밋

--- src/ex21jdbc/statement/DeleteSQL.java
+++ src/ex21jdbc/statement/DeleteSQL.java
@@ -10,6 +10,7 @@ public class DeleteSQL {
 	private Connection con;
 	private Statement stmt;
 	
+	//생성자에서는 드라이버에 대한 로드만 진행한다.
 	public DeleteSQL() {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
@@ -20,6 +21,7 @@ public class DeleteSQL {
 		}
 	}
 	
+	//Oracle DB에 대한 연결을 진행한다.
 	public void connect() {
 		try {
 			con = DriverManager.getConnection(
@@ -33,11 +35,19 @@ public class DeleteSQL {
 		}
 	}
 	
+	//DB연결, 쿼리준비, 쿼리전송, 쿼리실행을 위한 메소드
 	private void execute() {
+		
+		//여기서 실제 연결을 진행한다.
 		connect();
 		
 		try {
 			stmt = con.createStatement();
+			/*
+			test1 삭제시 제약조건위배 예외발생
+			test99와 같이 DB에 입력되지 않은 레코드 삭제시에는 예외없이
+				삭제되지 않음
+			*/
 			String query = "DELETE FROM member WHERE id='test2'";
 			int affected = stmt.executeUpdate(query);
 			
